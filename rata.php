44ac703ddf21ba3f85a437759588391b2d85726b
LINAULIA <51009783+LINAULIA@users.noreply.github.com>
2022-09-12 12:38:51 +0700

    Initial commit

---
+++ rata.php
@@ -0,0 +1,14 @@
+<?php
+$nilai1=790;
+$nilai2=483;
+$nilai3=281;
+$nilai4=224;
+$nilai5=198;
+$nilai6=60;
+$nilai7=698;
+$nilai8=400;
+$nilai9=709;
+$nilai10=168;
+$hasil=$nilai1+$nilai2+$nilai3+$nilai4+$nilai5+$nilai6+$nilai7+$nilai8+$nilai9+$nilai10;
+$rata=$hasil / 10;
+echo "Nilai rata - ratanya adalah $rata";
